d11d024fa502273e6f710ca1ea9a7943afd431f1
Pedro Pablo Muñoz Peñafiel <pedropablocampeon@gmail.com>
2022-12-14 08:51:37 +0100

    Final

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TestIBM
 Repository for viewnext technical test
+-Para ejecutar el programa habría que abrir una consola de comandos, dirigirnos a la carpeta del proyecto y desde esta ejecutar el siguiente comando:
+java -jar ejecutable.jar 5, donde 5 es el id_cliente que queramos introducir.
+
+-El fichero se genera en la raiz del proyecto y se llama "output.txt".
+
+-Las dependencias utilizadas han sido: MySQL Connector/J(Maven).
--- src/main/java/Test/TestIBM/src/main/java/technicalTest/Main.java
+++ src/main/java/Test/TestIBM/src/main/java/technicalTest/Main.java
@@ -1,47 +1,44 @@
 package Test.TestIBM.src.main.java.technicalTest;
 
-import java.io.BufferedWriter;
-import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
-
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/test";
-		String username = "root";
-		String password = "root";
+		//URL a la bbdd local, con usario y pwd creados previamente
+		String url = "jdbc:mysql://localhost:3702/test";
+		String username = "pedro";
+		String password = "admin";
+		
+		//Comprobacion si hay clientes asignados
 		if(args[0].contentEquals("5")||args[0].contentEquals("6")) {
 			
 			try {
+				//Conexion a la bbdd, prepared statement para almacenar la query, resulset para recoger los datos extraidos
 				Connection con= DriverManager.getConnection(url,username,password);
+				//El id_cliente es pasado por argumentos
 				PreparedStatement smt=con.prepareStatement("SELECT * FROM test.proveedores WHERE id_cliente ='"+args[0]+"'");
 				
 				ResultSet rs=smt.executeQuery();
-				
+				//ArrayList para almacenar los datos
 				ArrayList<Object> lista= new ArrayList<Object>();
 				
 				FileOutputStream fos = new FileOutputStream("output.txt");
 			    ObjectOutputStream oos = new ObjectOutputStream(fos); 
-			    
+			    //Bucle para rellenar la List con los datos extraidos
 				while(rs.next()) {
 					int idProveedor=rs.getInt("id_proveedor");
 					String nombre=rs.getString("nombre");
@@ -51,12 +48,11 @@ public class Main {
 					lista.add(nombre);
 					lista.add(fechaAlta);
 					lista.add(idCliente);
-					
+					//Escribo en el fichero ,los datos que existen en la lista
 					for(int i=0;i<lista.size();i++){
 				        oos.writeObject(lista.get(i));
 				    }
-			        
-			        
+
 					System.out.format("%s, %s, %s, %s,\n", idProveedor, nombre, fechaAlta, idCliente);
 				}
 				oos.close();
@@ -65,16 +61,11 @@ public class Main {
 				
 				e.printStackTrace();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else {System.out.println("El cliente no tiene proveedores asignados");}
-		
-		ArrayList<Object> lista= new ArrayList<Object>();
-		
+		}else {System.out.println("El cliente no tiene proveedores asignados");}	
 	}
 
 }
